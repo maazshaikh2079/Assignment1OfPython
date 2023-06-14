c5777d499004d746e91286c7fe3921daec0fbe76
maazshaikh2079 <124486797+maazshaikh2079@users.noreply.github.com>
2023-06-14 17:33:05 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,15 @@
+m1=float(input("Enter marks 1 : "));
+m2=float(input("Enter marks 2 : "));
+m3=float(input("Enter marks 3 : "));
+print("Marks 1       :",m1);
+print("Marks 2       :",m2);
+print("Marks 3       :",m3);
+print("Average of ",m1,", ",m2,", ",m3," is : ",((m1+m2+m3)/3),sep='');
+# Output:-
+# Enter marks 1 : 10
+# Enter marks 2 : 8
+# Enter marks 3 : 8
+# Marks 1       : 10.0
+# Marks 2       : 8.0
+# Marks 3       : 8.0
+# Average of 10.0, 8.0, 8.0 is : 8.666666666666666
